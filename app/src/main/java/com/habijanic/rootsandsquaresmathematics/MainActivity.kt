4f45113a38d613561e8bcf8011d92dfec15d1837
Amalia Habijanić <habijanic.ah@gmail.com>
2024-07-11 21:54:39 +0200

    Adding elements to activity_main.xml and MainActivity.kt. Adding primary colors to themes.xml.

--- app/src/main/java/com/habijanic/rootsandsquaresmathematics/MainActivity.kt
+++ app/src/main/java/com/habijanic/rootsandsquaresmathematics/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.habijanic.rootsandsquaresmathematics
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var numUpTo : EditText
+    lateinit var square : Button
+    lateinit var root : Button
+    var number = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        numUpTo = findViewById(R.id.editTextNumber)
+        square = findViewById(R.id.buttonSquares)
+        root = findViewById(R.id.buttonRoots)
+
+        square.setOnClickListener {
+
+            val input = numUpTo.text.toString()
+            if (input.isNullOrEmpty()){
+                Toast.makeText(applicationContext,"enter a number",Toast.LENGTH_LONG).show()
+            }
+            else{
+                number = Integer.parseInt(input)
+                Toast.makeText(applicationContext,"$number",Toast.LENGTH_LONG).show()
+            }
+
+        }
+        root.setOnClickListener {
+
+            val input = numUpTo.text.toString()
+            if (input.isNullOrEmpty()){
+                Toast.makeText(applicationContext,"enter a number",Toast.LENGTH_LONG).show()
+            }
+            else{
+                number = Integer.parseInt(input)
+                Toast.makeText(applicationContext,"$number",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 }

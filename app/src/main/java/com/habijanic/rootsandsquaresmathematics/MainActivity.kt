e6c16df5b945c3ed187627208232df9bf3f21f53
Amalia Habijanić <habijanic.ah@gmail.com>
2025-10-03 16:00:23 +0200

    Code cleanup

--- app/src/main/java/com/habijanic/rootsandsquaresmathematics/MainActivity.kt
+++ app/src/main/java/com/habijanic/rootsandsquaresmathematics/MainActivity.kt
@@ -2,27 +2,29 @@ package com.habijanic.rootsandsquaresmathematics
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var numUpTo : EditText
-    lateinit var add : Button
-    lateinit var sub : Button
-    lateinit var multiply : Button
-    lateinit var divide : Button
-    lateinit var square : Button
-    lateinit var root : Button
+    private lateinit var numUpTo : EditText
+    private lateinit var add : Button
+    private lateinit var sub : Button
+    private lateinit var multiply : Button
+    private lateinit var divide : Button
+    private lateinit var square : Button
+    private lateinit var root : Button
 
-    lateinit var exitButton : Button
-    var number = 0
+    private lateinit var exitButton : Button
+    private var number = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +36,11 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         // Remove background color of navigation bar
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        window.navigationBarColor = resources.getColor(android.R.color.transparent)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.navigationBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        window.navigationBarColor = ContextCompat.getColor(this, android.R.color.transparent)
 
 
 
@@ -82,7 +86,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun numberValidation(type: Int) {
         val input = numUpTo.text.toString()
-        if (input.isNullOrEmpty()){
+        if (input.isEmpty()){
             Toast.makeText(applicationContext,getString(R.string.enter_a_number),Toast.LENGTH_SHORT).show()
         }
         else if (Integer.parseInt(input) < 2){
